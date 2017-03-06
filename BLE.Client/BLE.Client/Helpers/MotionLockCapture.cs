37e648e2a8baebf0d64abcb66c591553de1b110b
Joshua Jarvis <jmj3886@rit.edu>
2017-03-06 14:32:06 -0500

    Added Intent handler

--- BLE.Client/BLE.Client/Helpers/MotionLockCapture.cs
+++ BLE.Client/BLE.Client/Helpers/MotionLockCapture.cs
@@ -1,9 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using MvvmCross.Platform;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
 using Acr.UserDialogs;
 using MvvmCross.Core.ViewModels;
-using MvvmCross.Platform;
+using Plugin.BLE.Abstractions;
 using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.EventArgs;
+using Plugin.BLE.Abstractions.Extensions;
 
 namespace BLE.Client.Helpers
 {
@@ -11,21 +17,37 @@ namespace BLE.Client.Helpers
     {
         private IDevice _device;
         private IService _buttons;
+        private ICharacteristic button;
         private IService _acelerometer;
         private IService _gyroscope;
+        private bool Intent = false;
 
         public IList<IService> Services { get; private set; }
         public MotionLockCapture(IDevice device)
         {
             _device = device;
             LoadServices();
+        }
+
+        private async void CapturePassword()
+        {
+            IList<ICharacteristic> characteristics = await _buttons.GetCharacteristicsAsync();
+            button = characteristics[0];
+            button.ValueUpdated -= IntentFound;
+            button.ValueUpdated += IntentFound;
+            await button.StartUpdatesAsync();
+        }
+
+        private async void LoadServices()
+        {
+            Services = await _device.GetServicesAsync();
             foreach (IService current_service in Services)
             {
-                if (current_service.Id.ToString().CompareTo("FFE1") == 0)
+                if (current_service.Id.ToString().Substring(0,8).CompareTo("0000ffe1") == 0)
                 {
                     _buttons = current_service;
                 }
-                else if (current_service.Id.ToString().CompareTo("AA51") == 0)
+                else if (current_service.Id.ToString().Substring(0, 8).CompareTo("0000aa51") == 0)
                 {
                     _gyroscope = current_service;
                 }
@@ -33,18 +55,18 @@ namespace BLE.Client.Helpers
             CapturePassword();
         }
 
-        private async void CapturePassword()
+        private void IntentFound(object sender, CharacteristicUpdatedEventArgs characteristicUpdatedEventArgs)
         {
-            IList<ICharacteristic> button = await _buttons.GetCharacteristicsAsync();
-            
+            if (button.Value.Equals(1) || button.Value.Equals(2) || button.Value.Equals(3))
+            {
+                Intent = true;
+                //COLLECT DATA                                                  `
+            }else if (button.Value.Equals(0) && Intent)
+            {
+                Intent = false;
+                //INTERPERET DATA FROM DEVICE
+            }
         }
-
-        private async void LoadServices()
-        {
-            Services = await _device.GetServicesAsync();
-        }
-
-
 
 
     }
