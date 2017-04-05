8f483a0c63d0b4cef04382565d87dbc4ce71bd99
blackpan2 <blackpan2@gmail.com>
2017-04-05 11:45:37 -0400

    Login view model button clicks now follows style and sends user to the intent page

--- BLE.Client/BLE.Client/ViewModels/LoginViewModel.cs
+++ BLE.Client/BLE.Client/ViewModels/LoginViewModel.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 using Plugin.BLE.Abstractions.Contracts;
 using Xamarin.Forms;
 using System.Windows.Input;
+using System.Linq;
+using MvvmCross.Core.ViewModels;
+using System.Collections.Generic;
 
 namespace BLE.Client.ViewModels
 {
@@ -11,32 +14,29 @@ namespace BLE.Client.ViewModels
 	{
 		protected readonly new IAdapter Adapter;
 
-		private LoginViewModel value1;
-
-		//public LoginViewModel ButtonSelect
-		//{
-		//	get
-		//	{
-		//		enterPassword();
-		//	}
-		//	set { if (value != null) value1 = value;}
-		//}
+		public ICommand EnterPassword { protected set; get; }
+		public ICommand DeviceListClick { protected set; get; }
 
 		public LoginViewModel(IAdapter adapter) : base(adapter)
 		{
 			Adapter = adapter;
 
-		}
-
-		public ICommand Click
-		{
-			get
+			EnterPassword = new Command((nothing) =>
 			{
-				return new Command(() =>
-				{
-					ShowViewModel<DeviceListViewModel>();
-				});
-			}
+			   string id = Adapter.ConnectedDevices.FirstOrDefault() != null ? this.Adapter.ConnectedDevices.FirstOrDefault().Id.ToString() : null;
+			   if (id != null)
+			   {
+					ShowViewModel<MotionLockEntryViewModel>(new MvxBundle(new Dictionary<string, string> { { DeviceIdKey, id } }));
+			   }
+			   else
+			   {
+					Application.Current.MainPage.DisplayAlert("No Device Connected", "You must connect to a device before entering password", "OK");
+			   }
+		    });
+			DeviceListClick = new Command((nothing) =>
+			{
+				ShowViewModel<DeviceListViewModel>();
+			});
 		}
 	}
 }
